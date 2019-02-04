e1a7e7f0a427142f1594ecf7ee0b3264391b80cb
cemremengu <cemremengu@gmail.com>
2019-02-04 11:53:11 +0300

    Added wrap-packer for single executable

--- publish.ps1
+++ publish.ps1
@@ -1,1 +1,2 @@
 dotnet publish -c Release -r win10-x64
+tools/windows-x64.warp-packer.exe --arch windows-x64 --input_dir tnsping/bin/Release/netcoreapp2.0/win10-x64/publish --exec tnsping.exe --output tnsping.exe
