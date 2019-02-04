f6c154696ed0655b95b0135ffc2a0820249850d7
cemremengu <cemremengu@gmail.com>
2019-02-04 11:04:02 +0300

    initial

---
+++ tnsping/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace tnsping
+{
+    using Oracle.ManagedDataAccess.Client;
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tns = args[0];
+            OracleConnection con = new OracleConnection(tns.ToString());
+
+            try
+            {
+                con.Open();
+                Console.WriteLine("Connected to Oracle Database {0}", con.ServerVersion);
+                Console.WriteLine("Press RETURN to exit.");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : {0}", ex);
+            }
+            finally
+            {
+                con.Dispose();
+            }
+        }
+    }
+}
